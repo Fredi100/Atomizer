60dbe8c7cdff51682003a267512c8a8f6c859dd0
Alfred Emsenhuber <fredi.emsenhuber@live.at>
2016-07-01 22:48:55 +0200

    Corrected Version Number

--- src/main/java/atomizer/lib/Constants.java
+++ src/main/java/atomizer/lib/Constants.java
@@ -8,7 +8,7 @@ package atomizer.lib;
 public class Constants {
 	public static final String MODID = "atomizer";
 	public static final String MODNAME = "Atomizer";
-	public static final String VERSION = "0.5.0";
+	public static final String VERSION = "Alpha-0.1";
 	public static final String CLIENT_PROXY_CLASS = "atomizer.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "atomizer.proxy.CommonProxy";
 	
